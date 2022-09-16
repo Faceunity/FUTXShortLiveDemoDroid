f26d06ff55310109b32c216559507b88fcdd1c5f
benyq <30992974+benyq@users.noreply.github.com>
2022-09-16 16:09:24 +0800

    FURenderkit 升级到 v8.3.0

--- faceunity/src/main/java/com/faceunity/nama/control/PropControlView.java
+++ faceunity/src/main/java/com/faceunity/nama/control/PropControlView.java
@@ -1,6 +1,8 @@
 package com.faceunity.nama.control;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
